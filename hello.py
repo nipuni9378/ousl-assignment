d404fb9ac3de37494beaffdb20348001fd93cb89
nipuni9378 <nuthpala789@gmail.com>
2024-12-24 13:11:42 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,27 @@
+text="""
+Computers have become an integral part of modern life. They
+have revolutionized various aspects of human existence, from
+communication and education to business and entertainment.
+The evolution of computers has led to significant advancements
+in technology, shaping the way we live and work.
+
+The history of computers dates back to the mid-20th century,
+from room-sized machines with limited capabilities to sleek,
+powerful devices that fit in the palm of our hands. Early
+computers were primarily used for calculations, but as
+technology progressed, they became more versatile, and capable
+of handling complex tasks and processing vast amounts of data.
+Applications in Various Sectors
+
+Computers play a crucial role in diverse fields such as medicine,
+engineering, and finance. In medicine, they aid in diagnosis,
+treatment planning, and research. Engineers rely on computers
+for design simulations and analysis. In finance, they facilitate
+transactions, automate processes, and manage data securely.
+
+In conclusion, computers have transformed the way live, work and
+interact with the world around us. Their impact is profound and 
+far-reaching shaping the trajectory of human progress. As 
+technology continues to advance, the role of computers in s
+society will only become more indispensable."""
+print(text)
